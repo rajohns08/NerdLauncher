7885ec8643cd962f1582c3863126f0d5934d144d
Adam Johns <robert.adam.johns@gmail.com>
2014-03-03 09:06:06 -0600

    sorting and getting labels of activities

--- NerdLauncher/src/main/java/com/bignerdranch/android/nerdlauncher/nerdlauncher/NerdLauncherActivity.java
+++ NerdLauncher/src/main/java/com/bignerdranch/android/nerdlauncher/nerdlauncher/NerdLauncherActivity.java
@@ -1,36 +1,15 @@
 package com.bignerdranch.android.nerdlauncher.nerdlauncher;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class NerdLauncherActivity extends ActionBarActivity {
+public class NerdLauncherActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nerd_launcher);
+    protected Fragment createFragment() {
+        return new NerdLauncherFragment();
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.nerd_launcher, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
---
+++ NerdLauncher/src/main/java/com/bignerdranch/android/nerdlauncher/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,54 @@
+package com.bignerdranch.android.nerdlauncher.nerdlauncher;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by rajohns on 2/28/14.
+ */
+public class NerdLauncherFragment extends ListFragment {
+    private static final String TAG = "NerdLauncherFragment";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+
+        Log.i(TAG, "I've found " + activities.size() + " activities.");
+
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo a, ResolveInfo b) {
+                PackageManager pm = getActivity().getPackageManager();
+                return String.CASE_INSENSITIVE_ORDER.compare(a.loadLabel(pm).toString(), b.loadLabel(pm).toString());
+            }
+        });
+
+        ArrayAdapter<ResolveInfo> adapter = new ArrayAdapter<ResolveInfo>(getActivity(), android.R.layout.simple_list_item_1, activities) {
+            public View getView(int pos, View convertView, ViewGroup parent) {
+                View v = super.getView(pos, convertView, parent);
+                TextView tv = (TextView)v;
+                ResolveInfo ri = getItem(pos);
+                tv.setText(ri.loadLabel(pm));
+                return v;
+            }
+        };
+    }
+}
